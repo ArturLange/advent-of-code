13a85ee8eae3e5766d9792b1315b63f7605839bb
Artur Lange <arturlange@poczta.fm>
2017-12-05 20:53:01 +0100

    Complete day 1

--- day1.py
+++ day1.py
@@ -1,19 +1,39 @@
-def get_captcha_score(number):
+from copy import deepcopy
+
+
+def get_captcha_score(number, places):
     score = 0
     string = list(str(number))
-    new_string = list(string.pop())
-    new_string.extend(string)
-    numbers = zip(str(number), new_string)
+    new_string = move_list(string, places)
+    numbers = zip(string, new_string)
     for pair in numbers:
         if pair[0] == pair[1]:
             score += int(pair[0])
     return score
 
 
-assert get_captcha_score(1111) == 4
-assert get_captcha_score(1122) == 3
-assert get_captcha_score(1234) == 0
-assert get_captcha_score(91212129) == 9
+def move_list(list_, number):
+    old_list = deepcopy(list_)
+    new_list = []
+    for _ in range(number):
+        new_list.append(old_list.pop())
+    new_list.reverse()
+    new_list.extend(old_list)
+    return new_list
+
+
+assert get_captcha_score(1111, 1) == 4
+assert get_captcha_score(1122, 1) == 3
+assert get_captcha_score(1234, 1) == 0
+assert get_captcha_score(91212129, 1) == 9
+
+assert get_captcha_score(1212, 2) == 6
+assert get_captcha_score(1221, 2) == 0
+assert get_captcha_score(123425, 3) == 4
+assert get_captcha_score(123123, 3) == 12
+assert get_captcha_score(12131415, 4) == 4
 
 with open('inputs/day1', 'r') as input_file:
-    print(get_captcha_score(input_file.readline()))
+    input_string = input_file.readline()
+    print(get_captcha_score(input_string, 1))
+    print(get_captcha_score(input_string, int(len(input_string) / 2)))
