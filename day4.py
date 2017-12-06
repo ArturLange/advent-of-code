596420a97d10410d9676ab181f0a6d80cd349a22
Artur Lange <arturlange@poczta.fm>
2017-12-06 18:37:30 +0100

    Solve day 4 part 2

--- day4.py
+++ day4.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from itertools import combinations
 
 
 def validate_passphrase(passphrase):
@@ -6,10 +7,26 @@ def validate_passphrase(passphrase):
     return all((count == 1 for count in counter.values()))
 
 
+def is_anagram(word1, word2):
+    w1 = list(word1)
+    w2 = list(word2)
+    w1.sort()
+    w2.sort()
+    return w1 == w2
+
+
+def validate_passphrase_v2(passphrase):
+    checks = combinations(passphrase.split(), 2)
+    return all((not is_anagram(word1, word2) for word1, word2 in checks))
+
+
 if __name__ == '__main__':
     with open('inputs/day4', 'r') as input_file:
         valid = 0
+        valid_v2 = 0
         for line in input_file.readlines():
             if validate_passphrase(line):
                 valid += 1
-        print(valid)
+            if validate_passphrase_v2(line):
+                valid_v2 += 1
+        print(valid, valid_v2)
--- test_day4.py
+++ test_day4.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day4 import validate_passphrase
+from day4 import validate_passphrase, validate_passphrase_v2
 
 
 class TestDay4(unittest.TestCase):
@@ -8,6 +8,13 @@ class TestDay4(unittest.TestCase):
         assert validate_passphrase('aa bb cc dd ee')
         assert not validate_passphrase('aa bb cc dd aa')
         assert validate_passphrase('aa bb cc dd aaa')
+
+    def test_validate_passphrase_v2(self):
+        assert validate_passphrase_v2('abcde fghij')
+        assert not validate_passphrase_v2('abcde xyz ecdab')
+        assert validate_passphrase_v2('a ab abc abd abf abj')
+        assert validate_passphrase_v2('iiii oiii ooii oooi oooo')
+        assert not validate_passphrase_v2('oiii ioii iioi iiio')
 
 
 if __name__ == '__main__':
