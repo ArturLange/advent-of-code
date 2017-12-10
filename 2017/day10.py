cb4cf5719b17120cc60019b6c1ce725ed2ecfdc9
Artur Lange <arturlange@poczta.fm>
2017-12-10 07:48:21 +0100

    Solve day 10 part 2

--- 2017/day10.py
+++ 2017/day10.py
@@ -1,7 +1,8 @@
-def hash_list(lengths, list_length=256):
-    current_position = 0
-    skip_size = 0
-    list_ = list(range(list_length))
+from functools import reduce
+
+
+def hash_list(lengths, list_, current_position=0, skip_size=0):
+    list_length = len(list_)
     for length in lengths:
         if length == 0:
             indexes = []
@@ -16,10 +17,34 @@ def hash_list(lengths, list_length=256):
                 list_[index] = sublist.pop()
         current_position = (current_position + length + skip_size) % list_length
         skip_size += 1
-    return list_
+    return list_, current_position, skip_size
+
+
+def to_hex(int_):
+    hexed = hex(int_)[2:]
+    return hexed if len(hexed) == 2 else '0' + hexed
+
+
+def hash_list_v2(strings, list_length=256, rounds=64):
+    lengths = [ord(char) for char in strings]
+    lengths.extend([17, 31, 73, 47, 23])
+    list_ = list(range(list_length))
+    current_position = 0
+    skip_size = 0
+    for _ in range(rounds):
+        list_, current_position, skip_size = hash_list(lengths, list_, current_position, skip_size)
+    result_string = ''
+    for part in range(16):
+        indexes = range(part * 16, part * 16 + 16)
+        hashed = reduce(lambda x, y: x ^ y, (list_[index] for index in indexes))
+        result_string += to_hex(hashed)
+    return result_string
 
 
 if __name__ == '__main__':
     lengths = [187, 254, 0, 81, 169, 219, 1, 190, 19, 102, 255, 56, 46, 32, 2, 216]
-    result = hash_list(lengths)
+    strings = '187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216'
+    list_256 = list(range(256))
+    result = hash_list(lengths, list_256)[0]
     print(result[0] * result[1])
+    print(hash_list_v2(strings))
--- 2017/test_day10.py
+++ 2017/test_day10.py
@@ -1,16 +1,20 @@
 import unittest
 
-from day10 import hash_list
+from day10 import hash_list, hash_list_v2
 
 
 class TestDay10(unittest.TestCase):
     def test_hash_list(self):
-        print(hash_list([187, 254, 0, 81, 169, 219, 1, 190, 19, 102, 255, 56, 46, 32, 2, 216]))
-        result = hash_list([3, 4, 1, 5], 5)
+        list_5 = list(range(5))
+        result = hash_list([3, 4, 1, 5], list_5)[0]
         assert result == [3, 4, 2, 1, 0]
         assert result[0] * result[1] == 12
 
-        assert hash_list([5, 2, 3, 1], 5) == [1, 4, 2, 3, 0]
+    def test_hash_list_v2(self):
+        assert hash_list_v2('') == 'a2582a3a0e66e6e86e3812dcb672a272'
+        assert hash_list_v2('AoC 2017') == '33efeb34ea91902bb2f59c9920caa6cd'
+        assert hash_list_v2('1,2,3') == '3efbe78a8d82f29979031a4aa0b16a9d'
+        assert hash_list_v2('1,2,4') == '63960835bcdc130f0b66d7ff4f6a5a8e'
 
 
 if __name__ == '__main__':
