975a0572d3b240d71c8cde0b16a30e360fe8b353
Artur H. Lange <arturlange@poczta.fm>
2017-12-20 13:35:12 +0100

    Improve day 18

--- 2017/day18.py
+++ 2017/day18.py
@@ -3,58 +3,75 @@ import string
 from typing import List
 
 
-def execute(operations: List[str]):
-    played = 0
-    sounds = {x: 0 for x in string.ascii_lowercase}
+class Execution:
+    def __init__(self, operations: List[str]):
+        self.operations = operations
+        self.sounds = {x: 0 for x in string.ascii_lowercase}
+        self.snd_re = re.compile(r'snd (?P<sound>[a-z])')
+        self.set_re = re.compile(r'set (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)')
+        self.add_re = re.compile(r'add (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)')
+        self.mul_re = re.compile(r'mul (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)')
+        self.mod_re = re.compile(r'mod (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)')
+        self.jgz_re = re.compile(r'jgz (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)')
+        self.rcv_re = re.compile(r'rcv (?P<sound>[a-z])')
 
-    def get_value(value):
+    def get_value(self, value):
         try:
             return int(value)
         except ValueError:
-            return sounds[value]
+            return self.sounds[value]
 
-    i = 0
-    while i < len(operations):
-        operation = operations[i]
-        if re.compile(r'snd (?P<sound>[a-z])').fullmatch(operation):
-            sound = re.compile(r'snd (?P<sound>[a-z])').fullmatch(operation).group('sound')
-            played = sounds[sound]
-        elif re.compile(r'set (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation):
-            match = re.compile(r'set (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation)
-            sound = match.group('sound')
-            value = match.group('value')
-            sounds[sound] = get_value(value)
-        elif re.compile(r'add (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation):
-            match = re.compile(r'add (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation)
-            sound = match.group('sound')
-            value = match.group('value')
-            sounds[sound] += get_value(value)
-        elif re.compile(r'mul (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation):
-            match = re.compile(r'mul (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation)
-            sound = match.group('sound')
-            value = match.group('value')
-            sounds[sound] *= get_value(value)
-        elif re.compile(r'mod (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation):
-            match = re.compile(r'mod (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation)
-            sound = match.group('sound')
-            value = match.group('value')
-            sounds[sound] %= get_value(value)
-        elif re.compile(r'jgz (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation):
-            match = re.compile(r'jgz (?P<sound>[a-z]) (?P<value>-?[a-z0-9]+)').fullmatch(operation)
-            sound = match.group('sound')
-            value = match.group('value')
-            if sounds[sound] > 0:
-                i += get_value(value)
-                i -= 1
-        elif re.compile(r'rcv (?P<sound>[a-z])').fullmatch(operation):
-            sound = re.compile(r'rcv (?P<sound>[a-z])').fullmatch(operation).group('sound')
-            if sounds[sound] > 0:
-                return played
-        i += 1
+    def execute(self):
+        played = 0
+
+        i = 0
+        while i < len(self.operations):
+            operation = self.operations[i]
+            if self.snd_re.fullmatch(operation):
+                sound = self.snd_re.fullmatch(operation).group('sound')
+                played = self.sounds[sound]
+            elif self.set_re.fullmatch(operation):
+                match = self.set_re.fullmatch(
+                    operation)
+                sound = match.group('sound')
+                value = match.group('value')
+                self.sounds[sound] = self.get_value(value)
+            elif self.add_re.fullmatch(operation):
+                match = self.add_re.fullmatch(
+                    operation)
+                sound = match.group('sound')
+                value = match.group('value')
+                self.sounds[sound] += self.get_value(value)
+            elif self.mul_re.fullmatch(operation):
+                match = self.mul_re.fullmatch(
+                    operation)
+                sound = match.group('sound')
+                value = match.group('value')
+                self.sounds[sound] *= self.get_value(value)
+            elif self.mod_re.fullmatch(operation):
+                match = self.mod_re.fullmatch(
+                    operation)
+                sound = match.group('sound')
+                value = match.group('value')
+                self.sounds[sound] %= self.get_value(value)
+            elif self.jgz_re.fullmatch(operation):
+                match = self.jgz_re.fullmatch(
+                    operation)
+                sound = match.group('sound')
+                value = match.group('value')
+                if self.sounds[sound] > 0:
+                    i += self.get_value(value)
+                    i -= 1
+            elif self.rcv_re.fullmatch(operation):
+                sound = self.rcv_re.fullmatch(operation).group('sound')
+                if self.sounds[sound] > 0:
+                    return played
+            i += 1
 
 
 if __name__ == '__main__':
     with open('inputs/day18', 'r') as input_file:
         operations = input_file.readlines()
         operations = [operation.replace('\n', '') for operation in operations]
-        print(execute(operations))
+        execution = Execution(operations)
+        print(execution.execute())
