72c2f3d7df401ed9ac1fa1ac99f9bfa026ac6b97
Artur H. Lange <ArturLange@poczta.fm>
2018-12-01 06:53:11 +0100

    Day 1 part 2

--- 2018/day1.py
+++ 2018/day1.py
@@ -1,6 +1,23 @@
+from itertools import cycle
+
 sum_ = 0
 with open('day1_input') as input_file:
     for line in input_file.readlines():
         sum_ += int(line)
 
 print(sum_)
+
+def part_2():
+    sum_ = 0
+    all_numbers = {sum_}
+    with open('day1_input') as input_file:
+        frequencies = cycle([int(line) for line in input_file.readlines()])
+    for number in frequencies:
+        sum_ += number
+        if sum_ in all_numbers:
+            return sum_
+        else:
+            all_numbers.add(sum_)
+
+print(part_2())
+
